e90821402ab804259ed7ea2cfe940aa8c93e705e
Pellegr1n1 <leandropellegini@gmail.com>
2023-10-31 16:57:42 -0300

    correções de bugs

--- lib/services/quiz_services.dart
+++ lib/services/quiz_services.dart
@@ -1,35 +1,29 @@
 import 'dart:math';
-import 'package:flutter/material.dart';
 import 'package:pokequiz/models/pokemon.dart';
 import 'package:pokequiz/utils/string_extension.dart';
 
 class QuizServices {
   final List<Pokemon> pokemonList;
-  int randomId;
+  int pokemon;
   static int correctAnswers = 0;
   static int incorrectAnswers = 0;
 
-  QuizServices(this.pokemonList, this.randomId);
+  QuizServices(this.pokemonList, this.pokemon);
 
   List<String> generateButtonNames() {
     int correctIndex = Random().nextInt(4);
     List<String> buttonNames = List.generate(4, (index) {
       if (index == correctIndex) {
-        return pokemonList[randomId].name.capitalize();
+        return pokemonList[pokemon].name.capitalize();
       } else {
         int randomIndex;
         do {
           randomIndex = Random().nextInt(pokemonList.length);
-        } while (randomIndex == randomId); // Garantir nomes diferentes
+        } while (randomIndex == pokemon); // Garantir nomes diferentes
         return pokemonList[randomIndex].name.capitalize();
       }
     });
     return buttonNames;
-  }
-
-  int updateRandomId() {
-    randomId = Random().nextInt(pokemonList.length);
-    return randomId;
   }
 
   bool checkPokemonCorrect(String selected, String correct) {
@@ -44,41 +38,7 @@ class QuizServices {
     }
   }
 
-  void nextPokemon(BuildContext context) async {
-    if (correctAnswers + incorrectAnswers < 10) {
-      print("Jogo Continua");
-    } else {
-      await showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return Stack(
-            children: [
-              ModalBarrier(
-                dismissible: false,
-                color: Colors.black.withOpacity(0.6),
-              ),
-              AlertDialog(
-                title: const Text("Quiz Concluído"),
-                content: Text(
-                    "Você completou o quiz com sucesso! Acertou: $correctAnswers Pokémon!"),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      resetQuiz();
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text("Restart"),
-                  ),
-                ],
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-
-  void resetQuiz() {
+  static void resetScore() {
     correctAnswers = 0;
     incorrectAnswers = 0;
   }
--- lib/widgets/pokemon_quiz.dart
+++ lib/widgets/pokemon_quiz.dart
@@ -17,34 +17,104 @@ class PokemonQuiz extends StatefulWidget {
 }
 
 class _PokemonQuizState extends State<PokemonQuiz> {
-  late int randomId = Random().nextInt(widget.pokemonList.length);
+  late int pokemonId;
+  bool isQuizFinished = false;
+
+  @override
+  void initState() {
+    super.initState();
+    pokemonId = Random().nextInt(widget.pokemonList.length);
+  }
+
+  // atualiza o pokemon
+  void updatePokemon() {
+    setState(() {
+      pokemonId = Random().nextInt(widget.pokemonList.length);
+    });
+  }
+
+  // reinicia o quiz
+  void restartQuiz() {
+    setState(() {
+      QuizServices.resetScore();
+      isQuizFinished = false;
+      updatePokemon();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var randomPokemon = widget.pokemonList[randomId];
-    QuizServices quizServices = QuizServices(widget.pokemonList, randomId);
+    var currentPokemon = widget.pokemonList[pokemonId];
+    QuizServices quizServices = QuizServices(widget.pokemonList, pokemonId);
     List<String> buttonNames = quizServices.generateButtonNames();
+
     return Padding(
       padding: const EdgeInsets.only(
-          top: 20.0, bottom: 12.0, left: 20.0, right: 20.0),
+        top: 20.0,
+        bottom: 12.0,
+        left: 20.0,
+        right: 20.0,
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          //pontuacao
           PokemonScore(
-              text01: "Acertos: ${QuizServices.correctAnswers}",
-              text02: "Erros: ${QuizServices.incorrectAnswers}"),
-          PokemonCardData(pokemon: randomPokemon),
+            text01: "Acertos: ${QuizServices.correctAnswers}",
+            text02: "Erros: ${QuizServices.incorrectAnswers}",
+          ),
+          //pokemon card
+          PokemonCardData(pokemon: currentPokemon),
+          //botoes
           for (var i = 0; i < 4; i++)
             PokemonButton(
               text: buttonNames[i],
               function: () {
-                setState(
-                  () {
-                    quizServices.checkPokemonCorrect(
-                        buttonNames[i], randomPokemon.name.capitalize());
-                  },
-                );
-                quizServices.nextPokemon(context);
+                setState(() {
+                  //verificacao da resposta
+                  quizServices.checkPokemonCorrect(
+                      buttonNames[i], currentPokemon.name.capitalize());
+                });
+                // verificacao se o quiz terminou
+                if (QuizServices.correctAnswers +
+                        QuizServices.incorrectAnswers >=
+                    10) {
+                  isQuizFinished = true;
+                }
+
+                if (isQuizFinished) {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Stack(
+                        children: [
+                          ModalBarrier(
+                            dismissible: false,
+                            color: Colors.black.withOpacity(0.6),
+                          ),
+                          AlertDialog(
+                            title: const Text("Quiz Concluído"),
+                            content: Text(
+                              "Você completou o quiz com sucesso! Acertou: ${QuizServices.correctAnswers} Pokémon!",
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  restartQuiz();
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text("Restart"),
+                              ),
+                            ],
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                } else {
+                  updatePokemon();
+                }
               },
             ),
         ],
